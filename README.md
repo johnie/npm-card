b613c2e232b4157ee75075aaf9061240e59be2cb
Johnie Hjelm <johnie@bambuser.com>
2022-07-16 23:09:37 +0200

    Add NPM card

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-It's me, Tierney!
+Here's Johnie!
 
 # Usage
 
 ## npm
+
 ```
-npx bitandbang
+npx johnie
 ```
 
 ## GitHub Package Registry
+
 Assuming you have the GitHub registry set up, you can use via npx:
+
 ```
-npx @bnb/card
+npx @johnie/card
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- build.js
+++ build.js
@@ -1,59 +1,76 @@
-'use strict'
+'use strict';
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
-const fs = require('fs')
-const path = require('path')
+const chalk = require('chalk');
+const boxen = require('boxen');
+const fs = require('fs');
+const path = require('path');
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
   borderStyle: 'round'
-}
+};
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('           Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  shorthandle: chalk.white('bnb'),
-  work: chalk.white('Staff Developer Advocate at Twilio'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('           Johnie Hjelm'),
+  handle: chalk.white('johnie'),
+  shorthandle: chalk.white('johnie'),
+  work: chalk.white('Technical Lead at Bambuser'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('johniehjelm'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~johnie'),
+  github: chalk.gray('https://github.com/') + chalk.green('johnie'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('johniehjelm'),
+  web: chalk.cyan('https://johnie.se'),
+  bio: chalk.magenta('https://jh.gy'),
+  npx: chalk.red('npx') + ' ' + chalk.white('johnie'),
   labelWork: chalk.white.bold('    Work:'),
   labelTwitter: chalk.white.bold(' Twitter:'),
   labelnpm: chalk.white.bold('     npm:'),
   labelGitHub: chalk.white.bold('  GitHub:'),
   labelLinkedIn: chalk.white.bold('LinkedIn:'),
   labelWeb: chalk.white.bold('     Web:'),
+  labelBio: chalk.white.bold('     Bio:'),
   labelCard: chalk.white.bold('    Card:')
-}
+};
 
 // Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const npming = `${data.labelnpm}  ${data.npm}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const newline = '\n';
+const heading = `${data.name} / ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const npming = `${data.labelnpm}  ${data.npm}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const bio = `${data.labelBio}  ${data.bio}`;
+const carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+const output =
+  heading + // data.name + data.handle
+  newline +
+  newline + // Add one whole blank line
+  working +
+  newline + // data.labelWork + data.work
+  twittering +
+  newline + // data.labelTwitter + data.twitter
+  npming +
+  newline + // data.labelnpm + data.npm
+  githubing +
+  newline + // data.labelGitHub + data.github
+  linkedining +
+  newline + // data.labelLinkedIn + data.linkedin
+  webing +
+  newline +
+  bio +
+  newline +
+  newline + // data.labelWeb + data.web
+  carding; // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(
+  path.join(__dirname, 'bin/output'),
+  chalk.green(boxen(output, options))
+);
